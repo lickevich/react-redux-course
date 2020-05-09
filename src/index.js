59d615f2bc51f36c48bd5b1d199d96fa385351cb
Roman Lickevich <r.lickevich@gmail.com>
2020-05-09 23:19:20 +0200

    react-redux-course init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux';
+import {compose, createStore, applyMiddleware} from "redux";
+import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import {rootReducer} from "./redux/rootReducer";
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {forbiddenWordsMiddleware} from './redux/middleware';
+import {sagaWatcher} from './redux/sagas';
 
-ReactDOM.render(
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, compose(
+  applyMiddleware(thunk, sagaMiddleware, forbiddenWordsMiddleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+sagaMiddleware.run(sagaWatcher);
+
+render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
